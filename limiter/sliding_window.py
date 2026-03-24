37905762d80fbf8c047919b71aae4311fc7cb664
anaykrishna <anaykrishna2022@gmail.com>
2026-03-24 21:34:50 +0530

    Added the Lua script to the sliding window redis executions

--- limiter/sliding_window.py
+++ limiter/sliding_window.py
@@ -3,6 +3,27 @@ import time
 
 r = redis.Redis("localhost", port=6379, decode_responses=True)
 
+SLIDING_WINDOW_SCRIPT = """
+local key          = KEYS[1]
+local now          = tonumber(ARGV[1])
+local window_start = tonumber(ARGV[2])
+local window_secs  = tonumber(ARGV[3])
+
+redis.call('ZREMRANGEBYSCORE', key, '-inf', window_start)
+
+local count = redis.call('ZCARD', key)
+
+redis.call('ZADD', key, now, tostring(now))
+
+redis.call('EXPIRE', key, window_secs)
+
+local oldest = redis.call('ZRANGE', key, 0, 0, 'WITHSCORES')
+
+return {count, oldest}
+"""
+
+sliding_window = r.register_script(SLIDING_WINDOW_SCRIPT)
+
 #Find out weather the api_key exisitsin the redis, if so return the meta data
 def get_key_metadata(api_key: str) -> dict | None:
   redis_key = f"apikey:{api_key}"
@@ -22,34 +43,20 @@ def get_key_metadata(api_key: str) -> dict | None:
 def is_allowed(identifier:str, limit:int, window_seconds:int) -> dict:
   # identifier : the identifier(IP)
   now = time.time()
-  window_start = now - window_seconds - 0.001
+  window_start = now - window_seconds 
 
-  pipe = r.pipeline()               # Mutiple redis commands to be executed as one
+  results = sliding_window(
+    keys=[identifier],
+    args=[now, window_start, window_seconds]
+  )
 
-  # Deleting all requests outside the current range
-  pipe.zremrangebyscore(identifier, "-inf", window_start)
-
-  # Count of number of requests from the identifier
-  pipe.zcard(identifier)
-
-  # Add the current identifier and timestamp
-  pipe.zadd(identifier, {str(now) : now})
-
-  # Set the identifier to expire after the current window
-  pipe.expire(identifier, window_seconds)
-
-  # The oldest request in the curretn window
-  pipe.zrange(identifier, 0, 0, withscores=True)
-
-  results = pipe.execute()
-
-  current_count = results[1]
-  oldest = results[4]
+  current_count = results[0]
+  oldest = results[1]
 
   allowed = current_count < limit
 
   if oldest:
-    oldest_timestap = oldest[0][1]
+    oldest_timestap = float(oldest[1])
     reset_after = (oldest_timestap + window_seconds) - now
   else:
     reset_after = window_seconds
